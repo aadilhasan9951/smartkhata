a940152b3e38d3f640ee4999c1577a68b979f082
SmartKhata Developer <smartkhata@example.com>
2026-04-23 20:19:07 +0530

    Set credentials false in CORS to fix wildcard origin issue

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
